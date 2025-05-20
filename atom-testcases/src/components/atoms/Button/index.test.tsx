abad5f3c5d515ea42dfee51203ade47ab9ec0194
Swetha <swetha.paramasivan@zemosolabs.com>
2025-05-20 17:42:15 +0530

    Updated the test files

--- atom-testcases/src/components/atoms/Button/index.test.tsx
+++ atom-testcases/src/components/atoms/Button/index.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/dom";
 import AppButton from "../Button/index";
 import { vi } from "vitest"; // this line ensures no TS errors
 
